67d136e9bd30ac1f971969ec93f5c4a7dadbcc3c
lenovo <lenovo@DESKTOP-BLIQA1L>
2021-03-23 00:33:22 -0700

    Add project files.

---
+++ KeyboardSampleProgram/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace KeyboardSampleProgram
+{
+    partial class KeyboardSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please press any of the letter on keyboard";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "r-red,  y-yellow , g-green,  m-maximise the form";
+            // 
+            // KeyboardSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(298, 218);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "KeyboardSample";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Keyboard Sample";
+            this.Load += new System.EventHandler(this.KeyboardSample_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyboardSample_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KeyboardSampleProgram/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyboardSampleProgram
+{
+    public partial class KeyboardSample : Form
+    {
+        public KeyboardSample()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyboardSample_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyboardSample_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                this.BackColor = Color.Red;
+
+            }
+            else if (e.KeyCode == Keys.G)
+            {
+                this.BackColor = Color.Green;
+
+            }
+            else if (e.KeyCode == Keys.Y)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else if (e.KeyCode == Keys.Q)
+            {
+                this.BackColor = Color.Black;
+                this.Text = "Queen";
+            }
+            else if(e.KeyCode==Keys.M)
+            {
+                this.WindowState = FormWindowState.Maximized;
+
+            }
+            else
+                MessageBox.Show("invalid");
+
+        }
+    }
+}
